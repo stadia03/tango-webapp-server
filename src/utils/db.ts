03b8d37a3d30fe2b4651eccebfd775722fa4cd4b
stadia03 <gaurab.working@gmail.com>
2025-06-24 20:52:48 +0530

    fix multiple db connection

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -6,13 +6,18 @@ if (!MONGODB_URI) {
   throw new Error("MONGODB_URI not defined");
 }
 
+let isConnected = false;
+
 async function dbConnect() {
+  if (isConnected) return;
+
   try {
     await mongoose.connect(MONGODB_URI, {
       bufferCommands: false,
+      maxPoolSize: 10,
       serverSelectionTimeoutMS: 30000,
-      maxPoolSize: 10, // Optional, for connection limit safety
     });
+    isConnected = true;
     console.log("✅ Connected to MongoDB");
   } catch (error) {
     console.error("❌ MongoDB connection error:", error);
